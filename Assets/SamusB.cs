e069a9d0f15b45056b4603c19eacaee0b14a6875
SmithAlDa <65830943+SmithAlDa@users.noreply.github.com>
2020-11-02 14:17:34 -0600

    Bounciness

---
+++ Assets/SamusB.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamusB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        const float MinImpulseForce = 3f;
+        const float MaxImpulseForce = 5f;
+
+        float angle = Random.Range(0, 2*Mathf.PI);
+
+        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        Rigidbody2D rgbd = GetComponent<Rigidbody2D>();
+        rgbd.AddForce(direction*magnitude, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
